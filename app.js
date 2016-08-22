57b6ab5fedfb48a021c23d3e21e9ec0ae05d0f5d
Pradeep Balachandran <pradeepb@in.ibm.com>
2016-08-22 22:48:38 +0530

    Sample form in Node

---
+++ app.js
@@ -0,0 +1,75 @@
+/*eslint-env node, express*/
+
+var port = process.env.VCAP_APP_PORT || 3000;
+var express = require("express");
+
+var app = express();
+
+
+var migratingProject;
+
+function resetMonitoring() {
+    migratingProject = "";
+}
+
+function beginMonitoring(project) {
+     // cleanup if we're re-setting the monitoring
+    if (migratingProject) {
+        resetMonitoring();
+    }
+    migratingProject = project;
+}
+
+
+app.get('/',
+    function (req, res) {
+        var welcomeResponse = "<HEAD>" +
+            "<title>Migration App</title>\n" +
+            "</HEAD>\n" +
+            "<BODY>\n" +
+            "<P>\n" +
+            "Hello! Welcome to the Migration app.<br>\n" + 
+            "Which Project would you like to migrate?\n" +
+            "</P>\n" +
+            "<FORM action=\"/monitor\" method=\"get\">\n" +
+            "<P>\n" +
+            "<INPUT type=\"text\" name=\"project\"><br><br>\n" +
+            "<INPUT type=\"submit\" value=\"Go\">\n" +
+            "</P>\n" + "</FORM>\n" + "</BODY>";
+        if (!migratingProject) {
+            res.send(welcomeResponse);
+        } else {
+            var monitoringResponse = "<HEAD>" +
+                "<META http-equiv=\"refresh\" content=\"5; URL=http://" +
+                req.headers.host +
+                "/\">\n" +
+                "<title>Migration App</title>\n" +
+                "</HEAD>\n" +
+                "<BODY>\n" +
+                "<P>\n" +
+                "Migrating project ...<br>\n" +
+                migratingProject + "<br>" +
+                "</P>\n" +
+                "<A href=\"/reset\">Monitor another phrase</A>\n" +
+                "</BODY>";
+            res.send(monitoringResponse);
+        }
+    });
+
+
+app.get('/monitor', function (req, res) {
+    beginMonitoring(req.query.project);
+    res.redirect(302, '/');
+});
+
+app.get('/reset', /* @callback */ function (req, res) {
+    resetMonitoring();
+    res.redirect(302, '/');
+});
+
+app.get('/test', /* @callback */ function (req, res) {
+    res.send(req.query);
+});
+
+app.listen(port);
+console.log("Server listening on port " + port);
